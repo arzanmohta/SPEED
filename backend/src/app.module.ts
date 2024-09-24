94d0155018b5c5f87654b75bca914a82b34b7a8c
arzanmohta <84154500+arzanmohta@users.noreply.github.com>
2024-09-24 15:06:22 +1200

    Set up database functionality for SPEED project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+/*
+
+Create a .env file in the backend src folder everytime you clone the project
+with the following code:
+
+MONGODB_URI=mongodb+srv://<username>:<password>@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority
+
+where <username> is your mongodb username and <password> is your mongodb password
+
+*/
